5f7df5c2e0aa8db7c379462422e80ae75c0a3489
Roy Zhang <r633462@trg518887.regence.com>
2018-10-30 21:56:26 -0700

    added enter key loop for successively calling csv rows

--- csv-txt-sender.py
+++ csv-txt-sender.py
@@ -2,11 +2,13 @@ from twilio.rest import Client
 import json
 import csv
 
-
 def get_config():
     with open('config.json') as f:
         return json.load(f)
 
+def block_then_call(row):
+    input("press enter to call " + row[1])
+    call(row[1], "hello " + row[0] + " plz vote citizen")
 
 def map_csv(csv_filename, map_row_fn):
     with open(csv_filename, 'r') as csvfile:
@@ -18,26 +20,15 @@ def map_csv(csv_filename, map_row_fn):
 def call(number, message):
     client.messages.create(to="+1"+number, from_="+13195354205", body=message)
 
-
-def call_row(row):
-    call(row[1], "hello " + row[0])
-
-
-def print_row(row):
-    print(row[0], " | ", row[1])
-
-
 def log(log_filename, msg):
     fo = open(log_filename, "a")
     fo.write(msg + "\n")
     fo.close()
 
-
 def io_loop_forever(input_fn):
     while True:
-        str = input("Enter your input: ");
+        str = input("Enter your input: ")
         input_fn("Received input is : ", str)
-
 
 def get_messages(client):
     all_messages = client.messages.list()
@@ -49,21 +40,16 @@ def get_messages(client):
 config = get_config()
 log(config["log_filename"], "starting the program")
 
-
 client = Client(config["account"], config["token"])
-map_csv(config["csv_filename"], print_row)
 
+map_csv(config["csv_filename"], block_then_call)
+
+
+#map_csv(config["csv_filename"], print_row)
 #map_csv(config["csv_filename"], call_row)
 
-
-get_messages(client)
-
-
-
 #get_messages(client)
-
 #io_loop_forever(print)
-
 
 #message = client.messages.create(to="+13194006347", from_="+13195354205",
 #                                 body="Hello there!")

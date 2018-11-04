34c049487b3e3397c222d0f93ec3779268dac405
Roy Zhang <r633462@trg518887.regence.com>
2018-11-04 11:11:41 -0800

    catches just 'stop'

--- text_filter.py
+++ text_filter.py
@@ -9,8 +9,11 @@ def has_wrong_number(body):
 def has_already_voted(body):
     return bool(re.compile(r'(?i)^.*(already voted|voted already).*$').search(body))
 
+def has_just_stop(body):
+    return bool(re.compile(r'(?i)^stop$').search(body))
+
 def has_stop_text(body):
-    return bool(re.compile(r'(?i)^.*(do not text|don\'t text|take me off|stop text|unsubscribe).*$').search(body))
+    return has_just_stop(body) or bool(re.compile(r'(?i)^.*(do not text|don\'t text|take me off|stop text|unsubscribe).*$').search(body))
 
 def has_swear_words(body):
     return bool(re.compile(r'(?i)^.*(fuck|shit|bitch|cunt|goddamn|biatch|crap|damn|prick|slut|whore).*$').search(body))

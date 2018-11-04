a86e5dd04285fc1dec411a5d0ce7992176ccf065
Roy Zhang <r633462@trg518887.regence.com>
2018-11-04 10:17:24 -0800

    fixed canned response referencing custom response

--- txt-reply-sender.py
+++ txt-reply-sender.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 import utils
+import text_filter
 import datetime
 import sys
 
@@ -10,8 +11,8 @@ client = Client(config["account"], config["token"])
 def count_inbound(all_messages):
     return len([message for message in all_messages if message.direction == "inbound"])
 
-def format_date(dateStr):
-    return dateStr.astimezone().strftime("%Y-%m-%d %I:%M %p")
+def format_date(date_str):
+    return date_str.astimezone().strftime("%Y-%m-%d %I:%M %p")
 
 def print_preamble(message):
     print("_________________________________________________________________\n"
@@ -38,7 +39,7 @@ def get_input():
 
 def between(numberStr, floor, ceiling):
     lastNumber = int(numberStr[-1:])
-    return lastNumber >= floor and lastNumber <= ceiling
+    return floor <= lastNumber <= ceiling
 
 def filter_numbers(messages, floor, ceiling):
     return [message for message in messages if between(message.from_, floor, ceiling) and message.direction == "inbound"]
@@ -46,8 +47,13 @@ def filter_numbers(messages, floor, ceiling):
 def time_what_hours_ago(hours_ago):
     return datetime.datetime.now() - datetime.timedelta(hours=hours_ago)
 
-def get_messages(client):
-    retrieved_messages = client.messages.list(limit=100)
+def txt_back(message, replyStr):
+    utils.txt(message.to, client, message.from_, replyStr)
+    utils.log(config["sent_logs_filename"], "sent " + replyStr + " to " + message.from_)
+    utils.delete_message(client, message.sid)
+
+def get_messages(twilio_client):
+    retrieved_messages = twilio_client.messages.list(limit=100)
     filtered_messages = filter_numbers(retrieved_messages, config["last_number_floor"], config["last_number_ceiling"])
     print('\nThere are {} inbound messages in your account ...\n'.format(count_inbound(filtered_messages)))
     for message in filtered_messages:
@@ -56,22 +62,19 @@ def get_messages(client):
             utils.log(config["received_logs_filename"], "got " + message.body + " from " + message.from_)
             response_selection = get_input()
             if response_selection == 'd':
-                utils.delete_message(client, message.sid)
+                utils.delete_message(twilio_client, message.sid)
                 print("> DELETING MESSAGE <\n"
                       "_________________________________________________________________\n\n")
             elif response_selection == '4':
                 custom_response = input("ENTER CUSTOM RESPONSE:\n")
                 print(f"\n> SENDING TO {message.from_} <\n{custom_response}\n"
                       "_________________________________________________________________\n\n")
-                utils.txt(message.to, client, message.from_, custom_response)
-                utils.log(config["sent_logs_filename"], "sent " + custom_response + " to " + message.from_)
-                utils.delete_message(client, message.sid)
+                txt_back(message, custom_response)
             elif response_selection in ['1', '2', '3']:
-                print(f"\n> SENDING TO {message.from_} <\n{config['canned_responses'][response_selection]}\n"
+                canned_response = config['canned_responses'][response_selection]
+                print(f"\n> SENDING TO {message.from_} <\n{canned_response}\n"
                       "_________________________________________________________________\n\n")
-                utils.txt(message.to, client, message.from_, custom_response)
-                utils.log(config["sent_logs_filename"], "sent " + custom_response + " to " + message.from_)
-                utils.delete_message(client, message.sid)
+                txt_back(message, canned_response)
             elif response_selection == 's':
                 print("> SKIPPING MESSAGE <\n"
                       "_________________________________________________________________\n\n")
